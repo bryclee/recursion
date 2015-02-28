f550436bbee3dcc665ee2070a2cbcc9e568334e6
Bryan Lee <pygmize.me@gmail.com>
2015-02-28 13:50:01 -0800

    implemented getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,27 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
+var getElementsByClassName = function(className, search
 ){
-  // your code here
+  console.log("*", className);
+  if (search == null)
+    search = document;
+  var res = [];
+  var children = search.childNodes;
+  
+  if (children.length == 0)
+    return [];
+  else {
+    for (var i = 0; i < children.length; i++){
+      if (children[i].classList == undefined) continue;
+      console.log(children[i]);
+      for (var j = 0; j < children[i].classList.length; j++){
+        if (className === children[i].classList[j])
+          res.push(children[i])
+      }
+      res = res.concat(getElementsByClassName(className, children[i]));
+    }
+  }
+  
+  return res;
 };
