3977a9eb96fde661c28b7d5d62c5c28313b99d27
bryclee <pygmize.me@gmail.com>
2015-03-02 12:33:25 -0800

    finished getElementsByClassName, stringifyJSON, started parseJSON

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,16 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ){
+	var results=[];
   // your code here
+  var checkClass = function(className, node) {
+  	if (node.classList.contains(className)) {
+  		results.push(node);
+  	}
+  	for (var i=0; i<node.children.length;i++) {
+  		checkClass(className, node.children[i]);
+  	}
+  }
+  checkClass(className, document.body);
+  return results;
 };
--- src/parseJSON.js
+++ src/parseJSON.js
@@ -4,4 +4,35 @@
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
   // your code goes here
+  if (json === 'true')
+  	return true;
+  if (json === 'false')
+  	return false;
+  if (json[0]==='"')
+  	return json.slice(1,json.length-1);
+  if (json[0]==='[') {
+  	var buffer = json.slice(1,json.length-1).split(',');
+  	if (buffer[0] === "")
+  		buffer = [];
+  	var results = [];
+  	for (var i=0; i<buffer.length; i++) {
+  		results.push(parseJSON(buffer[i].trim()));
+  	}
+  	return results;
+  }
+  if (json[0]==='{') {
+  	var buffer = json.slice(1,json.length-1).split(',');
+  	if (buffer[0] === "")
+  		buffer = [];
+  	var results = {};
+  	for (var i=0; i<buffer.length; i++) {
+  		var key = buffer[i].split(':')[0].trim();
+  		var value = buffer[i].split(':')[1].trim();
+  		results[parseJSON(key)] = parseJSON(value);
+  	}
+  	return results;
+  }
+  // is this array
+  // if so, split string along , (or ", ")
+  // recurse
 };
