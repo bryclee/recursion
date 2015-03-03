51be732cd0bb993959cacf56b14594535640015d
bryclee <pygmize.me@gmail.com>
2015-03-03 12:00:24 -0800

    Updated array parse case

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -4,6 +4,10 @@
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
   // your code goes here
+  if (!isNaN(parseFloat(json)))
+    return parseFloat(json);
+  if (json === 'null')
+    return null;
   if (json === 'true')
   	return true;
   if (json === 'false')
@@ -11,6 +15,7 @@ var parseJSON = function(json) {
   if (json[0]==='"')
   	return json.slice(1,json.length-1);
   if (json[0]==='[') {
+    /* INITIAL CODE
   	var buffer = json.slice(1,json.length-1).split(',');
   	if (buffer[0] === "")
   		buffer = [];
@@ -19,8 +24,41 @@ var parseJSON = function(json) {
   		results.push(parseJSON(buffer[i].trim()));
   	}
   	return results;
+    */
+    var openBrace = 0;
+    var openArray = 0;
+    var openQuote = 0;
+    var buffer = []; //buffer of letters
+    var parts = []; //finished pieces
+    for (var i = 1; i < json.length - 1; i++){
+      if (json[i] === '[')
+        openArray++;
+      if (json[i] === ']')
+        openArray--;
+      if (json[i] === '{')
+        openBrace++;
+      if (json[i] === '}')
+        openBrace--;
+      if (json[i] === '"')
+        openQuote = (openQuote + 1) % 2;
+      if (openBrace + openArray + openQuote === 0 && json[i] === ','){
+        parts.push(buffer.join(''));
+        buffer = [];
+      } else {
+        buffer.push(json[i]);
+      }
+    }
+    if (openBrace + openArray + openQuote !== 0)
+      return undefined; //error
+    parts.push(buffer.join(''));
+    var results = [];
+    for (var i = 0; i < parts.length; i++){
+      results.push(parseJSON(parts[i]));
+    }
+    return results;
   }
   if (json[0]==='{') {
+    /*
   	var buffer = json.slice(1,json.length-1).split(',');
   	if (buffer[0] === "")
   		buffer = [];
@@ -31,8 +69,48 @@ var parseJSON = function(json) {
   		results[parseJSON(key)] = parseJSON(value);
   	}
   	return results;
+    */
+    var openBrace = 0;
+    var openArray = 0;
+    var openQuote = 0;
+    var buffer = []; //buffer of letters
+    var parts = []; //finished pieces
+    for (var i = 1; i < json.length - 1; i++){
+      console.log(json[i]);
+      if (json[i] === '[')
+        openArray++;
+      if (json[i] === ']')
+        openArray--;
+      if (json[i] === '{')
+        openBrace++;
+      if (json[i] === '}')
+        openBrace--;
+      if (json[i] === '"')
+        openQuote = (openQuote + 1) % 2;
+      if (openBrace + openArray + openQuote === 0 && (json[i] === ',' || json[i] === ':')){
+        parts.push(buffer.join(''));
+        buffer = [];
+      } else {
+        buffer.push(json[i]);
+      }
+    }
+    if (openBrace + openArray + openQuote !== 0)
+      return undefined; //error
+    parts.push(buffer.join(''));
+    var results = {};
+    for (var i = 0; i < parts.length; i+=2){
+      results[parseJSON(parts[i])] = parseJSON(parts[i+1]);
+    }
+    return results;
   }
   // is this array
   // if so, split string along , (or ", ")
   // recurse
 };
+
+var getBlocks = function(strn){
+  var results = [];
+  for (var i = 0; i <strn.length; i++) {
+
+  }
+}
